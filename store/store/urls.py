971ec50bc0f0a16d92c8bb662e0c261932de9983
o.bagirov <o.bagirov.pl@gmail.com>
2023-05-10 22:11:44 -0700

    ver4.1: -Add urls -Add DRY

--- store/store/urls.py
+++ store/store/urls.py
@@ -3,13 +3,14 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from products.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls', namespace='index')),
 #    path('', index, name='index'),
     path('products/', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users')),
+    path('users/', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG:
