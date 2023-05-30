5a46d97a8739c91f5feb645ddb41602fee912cbd
o.bagirov <o.bagirov.pl@gmail.com>
2023-05-29 21:05:44 -0700

    ver6.1: -Paginator has been added -Filtres has been added

--- store/products/urls.py
+++ store/products/urls.py
@@ -5,7 +5,9 @@ app_name = 'products'
 
 urlpatterns = [
     path('products/', products, name='products'),
-    path('', index, name='index'),
+    path('', products, name='index'),
+    path('category/<int:category_id>/', products, name='category'),
+    path('page/<int:page_number>/', products, name='paginator'),
     path('baskets/add/<int:product_id>', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>', basket_remove, name='basket_remove'),
     ]
